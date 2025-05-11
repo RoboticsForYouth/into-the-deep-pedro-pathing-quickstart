551a5882e9f99b473a8df4c47e40d07cb4bb89fd
Dinesh  Bhat <dineshhbhat@gmail.com>
2025-05-11 12:04:47 -0400

    Merged az code into pedropathing

--- TeamCode/src/main/java/az/itd/teleop/PosStorage.java
+++
@@ -1,9 +0,0 @@
-package az.itd.teleop;
-
-public class PosStorage {
-
-    public static int initialSlidesPos = 0;
-    public static int initialArmPos = 0;
-
-
-}
--- TeamCode/src/main/java/az/itd/teleop/PoseStorage.java
+++
@@ -1,9 +0,0 @@
-package az.itd.teleop;//package org.firstinspires.ftc.teamcode.az.itd.teleop;
-//
-//
-//import com.arcrobotics.ftclib.geometry.Pose2d;
-//
-//public class PoseStorage{
-//    public static Pose2d currentPose = new Pose2d();
-//
-//}
